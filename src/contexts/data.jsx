630f563a5f6cdfae40bc8049fe6fd5b16594f2a9
Arthur Silva <arthurmatsilva@outlook.com>
2021-10-29 16:48:59 -0300

    feat: local storage

--- src/contexts/data.jsx
+++ src/contexts/data.jsx
@@ -1,12 +1,40 @@
-import { useState, createContext } from 'react';
+import { useState, useEffect, createContext } from 'react';
 
 export const DataContext = createContext({});
+
+let update = false;
 
 export function DataProvider({ children }) {
   const [general, setGeneral] = useState({});
   const [education, setEducation] = useState({});
   const [experience, setExperience] = useState({});
   const [expertise, setExpertise] = useState({});
+
+  useEffect(() => {
+    if (localStorage.getItem('data')) {
+      const data = JSON.parse(localStorage.getItem('data'));
+
+      setGeneral(data.general);
+      setEducation(data.education);
+      setExperience(data.experience);
+      setExpertise(data.expertise);
+    }
+
+    update = true;
+  }, []);
+
+  useEffect(() => {
+    if (update) {
+      const data = {
+        general,
+        education,
+        experience,
+        expertise
+      };
+  
+      localStorage.setItem('data', JSON.stringify(data));
+    }
+  }, [general, education, experience, expertise]);
 
   return (
     <DataContext.Provider value={{ 
